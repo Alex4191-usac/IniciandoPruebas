314b3d1a74acdcc51c0af7d2c84952353f31e81d
Alex4191-usac <3444601420101@ingenieria.usac.edu.gt>
2023-06-23 18:49:04 -0600

    mockFunction

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,72 @@
 const request = require('supertest');
 const app = require('../app');
+const pool = require('../config/db');
+const exp = require('constants');
 
+//mocking with jest db
+jest.mock('../config/db', () => ({
+    getConnection: jest.fn(),
+}));
 
-describe('GET /students', () => {
+test('should return 200 when get /students', async () => {
+    const mockRows = [
+        {
+            "stu_id": 123456,
+            "LastName": "DrinkWater",
+            "FirstName": "Walter",
+            "Address": "14 NE ST TX",
+            "City": "Texas"
+          },
+          {
+            "stu_id": 123457,
+            "LastName": "Gonzales",
+            "FirstName": "David",
+            "Address": "20 SW N TX",
+            "City": "Texas"
+          },
+          {
+            "stu_id": 123456,
+            "LastName": "Jucy",
+            "FirstName": "Lucy",
+            "Address": "14 NE ST TX",
+            "City": "Texas"
+          }
+    ];
 
-    it('should return a list of Students with a response of 200 OK', async () => {
-            const response = await request(app).get('/students');
-            expect(response.statusCode).toBe(200);
-            expect(response.body).toHaveLength(3);
+    //Mock the pool method implementations
+    pool.getConnection.mockResolvedValueOnce({
+        query: jest.fn().mockResolvedValueOnce(mockRows),
+        release: jest.fn()
     });
 
+    const res = await request(app).get('/students');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual(mockRows);
+
 });
+
+
+
+/*
+{
+      "stu_id": 123456,
+      "LastName": "DrinkWater",
+      "FirstName": "Walter",
+      "Address": "14 NE ST TX",
+      "City": "Texas"
+    },
+    {
+      "stu_id": 123457,
+      "LastName": "Gonzales",
+      "FirstName": "David",
+      "Address": "20 SW N TX",
+      "City": "Texas"
+    },
+    {
+      "stu_id": 123456,
+      "LastName": "Jucy",
+      "FirstName": "Lucy",
+      "Address": "14 NE ST TX",
+      "City": "Texas"
+    }
+ */
