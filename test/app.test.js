5018705e0f5e94aee1879e5b240c1c4261236f51
Alex4191-usac <68079950+Alex4191-usac@users.noreply.github.com>
2023-09-17 09:16:19 -0600

    [fix]bd info

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ const app = require('../app');
 describe('GET /students', () => {
 
     it('should return a list of Students with a response of 200 OK', async () => {
-            const response = await request(app).get('/students');
+            const response = await request(app).get('/users');
             expect(response.statusCode).toBe(200);
             expect(response.body).toHaveLength(3);
     });
